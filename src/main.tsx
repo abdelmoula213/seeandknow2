d1b04615b45e1d073c4c5e1009bda09e09a8471a
abdelmoula213 <abdelmoulaelmetkoul666@gmail.com>
2026-03-01 21:08:29 +0000

    Fix ThemeContext, Toaster, Tailwind styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css"; // <- this imports Tailwind
+import { ThemeProvider } from "./context/ThemeContext";
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+);
